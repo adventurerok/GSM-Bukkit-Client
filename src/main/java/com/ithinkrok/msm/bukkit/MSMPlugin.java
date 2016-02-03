571e035c832a63f4925418098f846987e959f366
paul <pauldurbz@gmail.com>
2016-02-03 21:11:24 +0000

    Now connects to a MSM server specified in the config

--- src/main/java/com/ithinkrok/msm/bukkit/MSMPlugin.java
+++ src/main/java/com/ithinkrok/msm/bukkit/MSMPlugin.java
@@ -1,9 +1,31 @@
 package com.ithinkrok.msm.bukkit;
 
+import com.google.common.net.HostAndPort;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by paul on 03/02/16.
  */
 public class MSMPlugin extends JavaPlugin {
+
+    private MSMClient client;
+
+    @Override
+    public void onEnable() {
+        getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
+            ConfigurationSection config = getConfig();
+
+            String hostname = config.getString("hostname");
+            int port = config.getInt("port", 30824);
+
+            HostAndPort address = HostAndPort.fromParts(hostname, port);
+
+
+            getLogger().info("Connecting to MSM Server at " + address);
+            client = new MSMClient(address);
+
+            client.start();
+        });
+    }
 }
