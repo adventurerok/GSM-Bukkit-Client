d330ee4e69d7cadf81f9fb100f97f13e638b13e9
paul <pauldurbz@gmail.com>
2016-02-07 21:14:38 +0000

    Register BungeeCord plugin channel

--- src/main/java/com/ithinkrok/msm/bukkit/MSMPlugin.java
+++ src/main/java/com/ithinkrok/msm/bukkit/MSMPlugin.java
@@ -9,8 +9,10 @@ import com.ithinkrok.msm.client.impl.MSMClient;
 import com.ithinkrok.msm.common.MinecraftServerType;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,36 +20,52 @@ import java.util.List;
 /**
  * Created by paul on 03/02/16.
  */
-public class MSMPlugin extends JavaPlugin {
+public class MSMPlugin extends JavaPlugin implements PluginMessageListener {
 
     private MSMClient client;
 
     @Override
     public void onEnable() {
+        ConfigurationSection config = getConfig();
+
+        boolean hasBungee = config.getBoolean("this_server.has_bungee");
+
+        if(hasBungee) {
+            getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+            getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", this);
+        }
+
+        addDefaultProtocols();
+
+        getServer().getScheduler().scheduleSyncDelayedTask(this, this::startMSMClient);
+    }
+
+    private void addDefaultProtocols() {
         addProtocol("MSMAutoUpdate", new ClientAutoUpdateProtocol(this));
 
         ClientAPIProtocol apiProtocol = new ClientAPIProtocol(this);
         getServer().getPluginManager().registerEvents(apiProtocol, this);
         addProtocol("MSMAPI", apiProtocol);
+    }
 
-        getServer().getScheduler().scheduleSyncDelayedTask(this, () -> {
-            ConfigurationSection config = getConfig();
+    private void startMSMClient() {
+        ConfigurationSection config = getConfig();
 
-            String hostname = config.getString("hostname");
-            int port = config.getInt("port", 30824);
+        String hostname = config.getString("hostname");
+        int port = config.getInt("port", 30824);
 
-            HostAndPort address = HostAndPort.fromParts(hostname, port);
+        HostAndPort address = HostAndPort.fromParts(hostname, port);
 
-            String serverName = config.getString("this_server.name");
-            boolean hasBungee = config.getBoolean("this_server.has_bungee");
+        String serverName = config.getString("this_server.name");
+        boolean hasBungee = config.getBoolean("this_server.has_bungee");
 
-            MinecraftServerInfo serverInfo = getServerInfo(serverName, hasBungee);
+        MinecraftServerInfo serverInfo = getServerInfo(serverName, hasBungee);
 
-            getLogger().info("Connecting to MSM Server at " + address);
-            client = new MSMClient(address, serverInfo);
+        getLogger().info("Connecting to MSM Server at " + address);
+        client = new MSMClient(address, serverInfo);
 
-            client.start();
-        });
+        client.start();
+
     }
 
     public static void addProtocol(String protocolName, ClientListener protocolListener) {
@@ -69,5 +87,10 @@ public class MSMPlugin extends JavaPlugin {
         }
 
         return new MinecraftServerInfo(serverType, serverName, hasBungee, maxPlayerCount, plugins);
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+
     }
 }
