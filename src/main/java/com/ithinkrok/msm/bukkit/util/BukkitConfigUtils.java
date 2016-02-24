1df1c52d3159582263dc059a9e32129b87591752
paul <pauldurbz@gmail.com>
2016-02-24 16:32:15 +0000

    Add support for string vectors and string locations

--- src/main/java/com/ithinkrok/msm/bukkit/util/BukkitConfigUtils.java
+++ src/main/java/com/ithinkrok/msm/bukkit/util/BukkitConfigUtils.java
@@ -14,11 +14,50 @@ import java.util.List;
 public class BukkitConfigUtils {
 
     public static Vector getVector(Config config, String path) {
-        return config.getConfigOrEmpty(path).saveObjectFields(new Vector());
+        if(config.isString(path)) {
+
+            String[] parts = config.getString(path).split(",");
+            if(parts.length < 3) return null;
+
+            try{
+                double x = Double.parseDouble(parts[0].trim());
+                double y = Double.parseDouble(parts[1].trim());
+                double z = Double.parseDouble(parts[2].trim());
+
+                return new Vector(x, y, z);
+            } catch (NumberFormatException ignored) {
+                return null;
+            }
+
+        } else if(config.isConfig(path)) {
+            return config.getConfigOrEmpty(path).saveObjectFields(new Vector());
+        } else return null;
     }
 
     public static Location getLocation(Config config, World world, String path) {
-        return config.getConfigOrEmpty(path).saveObjectFields(new Location(world, 0, 0, 0));
+        if(config.isString(path)){
+
+            String[] parts = config.getString(path).split(",");
+            if(parts.length < 3) return null;
+
+            try{
+                double x = Double.parseDouble(parts[0].trim());
+                double y = Double.parseDouble(parts[1].trim());
+                double z = Double.parseDouble(parts[2].trim());
+
+                if(parts.length < 5) return new Location(world, x, y, z);
+
+                double yaw = Double.parseDouble(parts[3].trim());
+                double pitch = Double.parseDouble(parts[4].trim());
+
+                return new Location(world, x, y, z, (float) yaw, (float) pitch);
+            } catch (NumberFormatException ignored) {
+                return null;
+            }
+
+        } else if(config.isConfig(path)) {
+            return config.getConfigOrEmpty(path).saveObjectFields(new Location(world, 0, 0, 0));
+        } else return null;
     }
 
     public static List<Vector> getVectorList(Config config, String path) {
