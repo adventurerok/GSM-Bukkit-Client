499f8bb3b655b325bb8e93e9ed65b6ab9c0caa42
paul <pauldurbz@gmail.com>
2016-03-09 17:16:56 +0000

    Tab completion support for gsm commands

--- src/main/java/com/ithinkrok/msm/bukkit/tabcomplete/TabCompleteListener.java
+++ src/main/java/com/ithinkrok/msm/bukkit/tabcomplete/TabCompleteListener.java
@@ -12,6 +12,7 @@ import org.bukkit.plugin.Plugin;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
 
 /**
  * Created by paul on 09/03/16.
@@ -25,7 +26,8 @@ public class TabCompleteListener extends PacketAdapter {
 
     private final String[] EMPTY_STRING_ARRAY = new String[0];
 
-    public TabCompleteListener(Plugin plugin, Map<String, CommandInfo> commandMap, Map<String, Set<String>> tabCompletionSets) {
+    public TabCompleteListener(Plugin plugin, Map<String, CommandInfo> commandMap,
+                               Map<String, Set<String>> tabCompletionSets) {
         super(plugin, ListenerPriority.NORMAL, PacketType.Play.Client.TAB_COMPLETE,
                 PacketType.Play.Server.TAB_COMPLETE);
         this.commandMap = commandMap;
@@ -52,23 +54,72 @@ public class TabCompleteListener extends PacketAdapter {
 
         Set<String> text = new TreeSet<>(Arrays.asList(packet.getText()));
 
-        if(!message.contains(" ") && message.startsWith("/")) {
-            String commandStub = message.substring(1).toLowerCase();
+        if (message.startsWith("/")) {
+            if (!message.contains(" ")) {
+                String commandStub = message.substring(1).toLowerCase();
 
-            addCommandNameCompletions(player, text, commandStub);
+                addCommandNameCompletions(player, commandStub, text);
+            } else {
+                String[] parts = message.split(" ");
+
+                String commandName = parts[0].substring(1).toLowerCase();
+                CommandInfo command = commandMap.get(commandName);
+                if (command == null) return;
+
+                if (command.getPermission() != null && !player.hasPermission(command.getPermission())) {
+                    return;
+                }
+
+                StringBuilder middle = new StringBuilder();
+
+                for (int index = 1; index < parts.length - 1; ++index) {
+                    if (middle.length() > 0) middle.append(" ");
+
+                    middle.append(parts[index]);
+                }
+
+                addCommandPartCompletions(command, middle.toString(), parts[parts.length - 1], text);
+            }
         }
 
         packet.setText(text.toArray(EMPTY_STRING_ARRAY));
     }
 
-    public void addCommandNameCompletions(Player player, Set<String> text, String commandStub) {
-        for(CommandInfo commandInfo : commandMap.values()) {
-            if(commandInfo.getPermission() != null && !player.hasPermission(commandInfo.getPermission())){
+    public void addCommandNameCompletions(Player player, String commandStub, Set<String> text) {
+        for (CommandInfo commandInfo : commandMap.values()) {
+            if (commandInfo.getPermission() != null && !player.hasPermission(commandInfo.getPermission())) {
                 continue;
             }
 
-            if(commandInfo.getName().startsWith(commandStub)) {
+            if (commandInfo.getName().startsWith(commandStub)) {
                 text.add("/" + commandInfo.getName());
+            }
+        }
+    }
+
+    private void addCommandPartCompletions(CommandInfo command, String middle, String end, Set<String> text) {
+        for (Map.Entry<Pattern, List<String>> tabCompletion : command.getTabCompletion().entrySet()) {
+            if (!tabCompletion.getKey().matcher(middle).matches()) continue;
+
+            for (String item : tabCompletion.getValue()) {
+                if (!item.startsWith("#")) {
+                    if (item.startsWith(end)) {
+                        text.add(item);
+                    }
+                    continue;
+                }
+
+                String setName = item.substring(1);
+                if (setName.isEmpty()) continue;
+
+                Set<String> set = tabCompletionSets.get(setName);
+                if (set == null) continue;
+
+                for (String setItem : set) {
+                    if (setItem.startsWith(end)) {
+                        text.add(setItem);
+                    }
+                }
             }
         }
     }
